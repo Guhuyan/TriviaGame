0ac0d0bb1b38899250f2398581a804f3a94b8e02
Gue <guehyang@gmail.com>
2019-07-10 00:32:28 -0700

    pseudo coding done

---
+++ assets/javascript/app.js
@@ -0,0 +1,118 @@
+/*
+Which of the following is correct?
+jQuery is a Java Library
+jQuery is a JavaScript Library (correct)
+jQuery is a JSON Library
+
+What scripting language is jQuery written in?
+Java
+JavaScript (correct)
+C++
+C#
+
+Is jQuery a library for client scripting or server scripting?
+Server scripting  
+Client scripting (correct)
+
+jQuery uses CSS selectors to select elements?
+True (correct)
+False
+
+Which sign does jQuery use as a shortcut for jQuery?
+the % sign
+the $ sign (correct)
+the ? Sign
+
+Look at the following selector: $("div"). What does it select?
+The first div element
+All div elements (correct)
+
+Look at the following selector: $("div p"). What does it select?
+The first p element inside a div element
+All p elements inside a div element (correct)
+All div elements with a p element
+
+With jQuery, look at the following selector: $("div.intro"). What does it select?
+The first div element with id="intro"
+The first div element with class="intro"
+All div elements with id="intro"
+All div elements with class="intro" (correct)
+
+Look at the following selector: $("p#intro"). What does it select?
+The p element with id="intro" (correct)
+The p element with class="intro"
+All p elements with id="intro"
+All p elements with class="intro"
+
+Which jQuery method is used to set one or more style properties for selected elements?
+style()
+css() (correct)
+html()
+
+Which jQuery method is used to hide selected elements?
+hide() (correct)
+hidden()
+display(none)
+visible(false)
+
+Which jQuery method is used to remove selected elements?
+remove()
+detach()
+Both A & B (correct)
+None of the above
+
+Which jQuery function is used to prevent code from running, before the document is finished loading?
+$(document).ready() (correct)
+$(document).load()
+$(body).onReady()
+$(body).onload()
+
+Which jQuery method returns the direct parent element of the selected element?
+ancestor()
+ancestors()
+parent() (correct)
+parents()
+
+Which jQuery method should be used to deal with name conflicts?
+conflict()
+nameConflict()
+noConflict() (correct)
+noNameConflict()
+
+Which jQuery method is used to switch between adding/removing one or more classes (for CSS) from selected elements?
+switch()
+altClass()
+toggleClass() (correct)
+switchClass()
+
+Is it possible to use jQuery together with AJAX?
+Yes (correct)
+No
+
+Which jQuery method is used to perform an asynchronous HTTP request?
+jQuery.ajaxAsync()
+jQuery.ajax() (correct)
+jQuery.ajaxSetup()
+*/
+
+//Declare two variables to keep track of the wrong/right answers
+
+//Declare an array to hold objects.  Each object has a question and 2 or four question.
+
+//Declare a variable and set it equal to a timer
+
+//Run through the object's length.  The first is always the question.
+//Pass the question to the appropriate question div.
+//Make a div for each of the answer.
+//Add eventListener
+//Set if statements to determine if the player choice is correct
+//If correct, alert that the player's choice was right or announce within the html by creating a new div in a separate html section (will have to create this section)
+//Add +1 to wrong variable count
+//If wrong, highlight the correct answer and announce that the player's choice was not correct
+//Set timeout to clear the message(s)
+//Restart the game
+
+//Make a reset function
+
+//Start the game when the start button is pressed
+//Run reset function once
